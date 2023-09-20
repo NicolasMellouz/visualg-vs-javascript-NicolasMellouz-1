6ee9603ae036478604addf4266e4be4ccb96ec8b
Nicolas Mello <127401029+NicolasMellouz@users.noreply.github.com>
2023-09-20 12:43:52 -0300

    Add files via upload

---
+++ home/scripthome.js
@@ -0,0 +1,9 @@
+let usuario =JSON.parse(localStorage.getItem("home"));
+document.getElementById("tituto)").innerHTML = "Bem vindo, "+usuario.login
+function home() {
+    window.location.href = "../index.html";
+    }
+    function index() {
+        window.location.href = "../index.html";
+        }
+    
